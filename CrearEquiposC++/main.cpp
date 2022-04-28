977e1bcd7d4ad5ec036e243789453cb7af5000b1
Roberta Saldana Farias <a00827939@itesm.mx>
2022-04-28 15:35:41 -0500

    avance crear equipos

--- CrearEquiposC++/main.cpp
+++ CrearEquiposC++/main.cpp
@@ -7,11 +7,36 @@ using namespace std;
 
 
 int main() {
-    vector<EmpProj> empProj;
+    vector< vector<EmpProj> > empProj;
+    vector<EmpProj> auxVecEmpTeam;
     vector<EmpTeam> empTeam;
     EmpProj auxEmpProj;
     EmpTeam auxEmpTeam;
+    auxEmpProj.SetEmpProj(1, 1, true, 1);
 
-    
+    // crear datos falsos
+    //role: 0=leader, 1=peer, 2=team
+    //projrole: 1=leader
+    int cont = 1;
+    int projRole = 0;
+    bool compHr = true;
+
+    for (int i = 1; i < 6; i++){
+        for (int j = 0; j<10; j++){
+            if (j==0){
+                projRole = 1;
+            } else {
+                projRole = 0;
+            }
+            if (j == 7){
+                compHr = false;
+            } else {
+                projRole = true;
+            }
+            auxEmpProj.SetEmpProj(cont, projRole, compHr, i);
+        }
+    }
+
+
 
 }
