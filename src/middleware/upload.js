4a585f69b0ffffd4fc5152ec5336e64b404d1896
Jessica Trevino <jessy.trevi00@gmail.com>
2022-05-03 14:32:47 -0500

    recibe archivo :)

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -16,8 +16,9 @@ var storage = multer.diskStorage({
     },
     filename: (req, file, cb) => {
       console.log(file.originalname);
-    //   cb(null, `${Date.now()}-bezkoder-${file.originalname}`);
-    cb(null, `${Date.now()}$-{file.originalname}`);
+      // cb(null, `${Date.now()}-bezkoder-${file.originalname}`);
+      cb(null, `${file.originalname}`);
+    // cb(null, `${Date.now()}$-{file.originalname}`);
     },
   });
   var uploadFile = multer({ storage: storage, fileFilter: excelFilter });
