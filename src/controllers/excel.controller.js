46eca6e83c3282cf921fe21545557220703050d5
melissamtzm <a00827008@itesm.mx>
2022-06-02 17:42:41 -0500

    remove request

--- src/controllers/excel.controller.js
+++ src/controllers/excel.controller.js
@@ -63,9 +63,9 @@ const upload = async (req, res) => {
   let orphans = [];
   let hoursToComplete = 40;
 
-  let path = '/Users/robertasaldana/Desktop/IPSCentralBackend/src/resources/static/assets/uploads/equipos.xlsx'
+  //let path = '/Users/robertasaldana/Desktop/IPSCentralBackend/src/resources/static/assets/uploads/equipos.xlsx'
   //let path = '/Users/jessicatrevino/Desktop/itesm/TC3005/reto/IPSCentralBackend/IPSCentralBackend/src/resources/static/assets/uploads/equipos.xlsx';
-  //let path = '/Users/melissa/Documents/tec/back6/IPSCentralBackend/src/resources/static/assets/uploads/equipos.xlsx';
+  let path = '/Users/melissa/Documents/tec/back6/IPSCentralBackend/src/resources/static/assets/uploads/equipos.xlsx';
 
   readXlsxFile(path).then(async (rows) => {
     //se salta los headers
@@ -318,7 +318,7 @@ const upload = async (req, res) => {
         } else if (user.role == 'Peer') {
           teamRole = 1; // peer
         }
-
+        //console.log(teamKey.id);
         const tempEmpTeam = await Employee_Team.create({ role_member: teamRole, status_member: 0, id_employee: empUser.id, id_team: teamKey.id });
       })
     }
@@ -430,6 +430,13 @@ const approveHR = async(req, res) => {
   res.status(200).send({message: "approve by HR successful"});
 };
 
+const requestRemove = async(req, res) => {
+	//console.log(req.body);	
+  const resultado = await db.sequelize.query(`EXEC REMOVEREQUEST :motive, :id_emp_mod, :type, :id_emp_req, :status, :title, :id_employee_teams`, 
+  {replacements: { motive: req.body.motive, id_emp_mod: req.body.id_emp_mod, type: req.body.type, id_emp_req: req.body.id_emp_req, status: req.body.status, title: req.body.title, id_employee_teams: req.body.id_employee_teams }})
+  res.status(200).send({message: "post request successful"});
+}; 
+
 
 
 
@@ -448,7 +455,8 @@ module.exports = {
   getOrphanTeams,
   requestAdd: requestAdd,
   removeHR: removeHR,
-  approveHR: approveHR
+  approveHR: approveHR,
+  requestRemove: requestRemove
 };
 
 
--- src/routes/tutorial.js
+++ src/routes/tutorial.js
@@ -20,6 +20,7 @@ let routes = (app) => {
     router.post("/requestAdd", excelController.requestAdd);
     router.post("/removeHR", excelController.removeHR);
     router.post("/approveHR", excelController.approveHR);
+    router.post("/requestRemove", excelController.requestRemove);
 
 
     // router.route('/requestAdd').post((request, response) => {
